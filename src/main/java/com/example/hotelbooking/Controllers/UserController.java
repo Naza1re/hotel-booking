853e2f0820d606c4d714f8664993991559b5c0b1
Naza1re <mcarim@mail.ru>
2023-09-06 22:56:17 +0300

    add User integration

--- src/main/java/com/example/hotelbooking/Controllers/UserController.java
+++ src/main/java/com/example/hotelbooking/Controllers/UserController.java
@@ -1,13 +1,19 @@
 package com.example.hotelbooking.Controllers;
 
 import com.example.hotelbooking.DataBaseConnection.DBSession;
+import com.example.hotelbooking.DataBaseConnection.UserDB;
 import com.example.hotelbooking.Models.User;
 import jakarta.persistence.Entity;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -24,6 +30,31 @@ public class UserController {
         session.getTransaction().commit();
         return "main";
     }
+
+    @GetMapping("/allUsers")
+    public String allUsers(Model model){
+        Session session = DBSession.getSession();
+        session.beginTransaction();
+        Query<User> query = session.createQuery("From User", User.class);
+        List<User> users = query.list();
+        model.addAttribute("users",users);
+        session.getTransaction().commit();
+        return "all-users";
+    }
+
+    @GetMapping("/user{id}")
+    public String profile(@PathVariable Long id,Model model){
+        Session session = DBSession.getSession();
+        session.beginTransaction();
+        User user = UserDB.getUserById(id);
+        
+        model.addAttribute("user",user);
+        session.getTransaction().commit();
+        return "user-details";
+    }
+
+
+
 
 
 
--- src/main/java/com/example/hotelbooking/Models/User.java
+++ src/main/java/com/example/hotelbooking/Models/User.java
@@ -1,21 +1,54 @@
 package com.example.hotelbooking.Models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "user")
 public class User {
     @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public String getName() {
+        return name;
+    }
+    public User(){
+    }
+
+    public User(String name, String phoneNumber, String mail) {
+        this.name = name;
+        this.phone_Number = phoneNumber;
+        this.mail = mail;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phone_Number;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phone_Number = phoneNumber;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
 
     @Column(name = "name")
     private String name;
+    @Column(name = "phone_Number")
+    private String phone_Number;
 
-
+    @Column(name = "mail")
+    private String mail;
 
     public void setId(Long id) {
         this.id = id;
